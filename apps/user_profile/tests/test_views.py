dd3d79bab6dd77f3d8b72239511844cc87ee7003
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-27 16:01:16 +0300

    ticket:1 added test, IndexView returns hard coded data

--- apps/user_profile/tests/test_views.py
+++ apps/user_profile/tests/test_views.py
@@ -7,3 +7,18 @@ class IndexViewTests(TestCase):
         """Index view uses index.html"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_index_view_returns_hardcoded_data(self):
+        """Index view returns hardcoded data"""
+        data = {
+            "first_name": "Denis",
+            "last_name": "Svistunov",
+            "date_birth": "1991-04-19",
+            "bio": "Lorem ipsum dolor sit amet.",
+            "email": "d.svistunov1991@gmail.com",
+            "jabber": "d.svistunov@42cc.co",
+            "skype": "d.svistunov",
+            "other_contacts": "+380632028013"
+        }
+        response = self.client.get('/')
+        self.assertEqual(response.context['data'], data)
