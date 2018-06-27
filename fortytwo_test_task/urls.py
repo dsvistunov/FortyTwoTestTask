22b1fafe16c1564be72d1ef639765873ef5427ac
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-27 15:45:08 +0300

    ticket:1 maked routing for IndexView

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from apps.user_profile import urls as biography_urls
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,5 +10,6 @@ urlpatterns = patterns(
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include(biography_urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
