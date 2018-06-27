10da6cf7bd914d4d54126fd8b4b02d87f8eae635
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-27 16:22:09 +0300

    ticket:1 fixed test

--- apps/user_profile/tests/test_models.py
+++ apps/user_profile/tests/test_models.py
@@ -14,4 +14,16 @@ class ProfileModelTests(TestCase):
         self.assertEqual(profile.jabber, 'd.svistunov@42cc.co')
         self.assertEqual(profile.skype, 'd.svistunov')
         self.assertEqual(profile.other_contacts, '+380632028013')
-        self.assertEqual(profile.bio, 'Lorem ipsum dolor sit amet.')
+        self.assertEqual(
+            profile.bio,
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+            'Integer imperdiet sit amet nulla in tristique. Nam ullamc'
+            'orper velit id venenatis dapibus. Nullam sit amet feugiat'
+            ' libero, vitae molestie ligula. Morbi pretium, ante id bl'
+            'andit maximus, lectus felis suscipit tellus, a posuere es'
+            't odio at ipsum. Orci varius natoque penatibus et magnis '
+            'dis parturient montes, nascetur ridiculus mus. Fusce fini'
+            'bus sollicitudin eros, id gravida velit lacinia at. Nulla'
+            'm pulvinar turpis vel lorem molestie, eget mattis diam da'
+            'pibus.'
+        )
