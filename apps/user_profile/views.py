424ec411146bfb3abcdc226a40ab9cc5674ae2e9
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-27 21:25:25 +0300

    ticket:3 added RequestsView, returns 10 requests

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,5 +1,7 @@
+from django.core import serializers
 from django.views.generic.base import TemplateView
-from .models import Profile
+from django.http import HttpResponse
+from .models import Profile, Request
 
 
 class IndexView(TemplateView):
@@ -12,6 +14,31 @@ class IndexView(TemplateView):
         return context
 
 
-class RequestsView(TemplateView):
+class JSONResponseMixin(object):
+
+    def render_to_json_response(self, context, **response_kwargs):
+        return HttpResponse(
+            self.convert_context_to_json(context),
+            content_type='application/json',
+            **response_kwargs
+        )
+
+    def convert_context_to_json(self, context):
+        return serializers.serialize('json', context['data'])
+
+
+class RequestsView(JSONResponseMixin, TemplateView):
 
     template_name = 'requests.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RequestsView, self).get_context_data(**kwargs)
+        context['data'] = Request.objects.order_by('-added')[:10]
+        return context
+
+    def get(self, request, *args, **kwargs):
+        response = super(RequestsView, self).get(request, *args, **kwargs)
+        if request.is_ajax():
+            context = self.get_context_data(**kwargs)
+            return self.render_to_json_response(context)
+        return response
