5fb46f6ae31a41237826a8310faaeaf9f6e43733
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-27 16:02:10 +0300

    ticket:1 IndexView returns hard coded data

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -4,3 +4,21 @@ from django.views.generic.base import TemplateView
 class IndexView(TemplateView):
 
     template_name = 'index.html'
+
+    def __init__(self, **kwargs):
+        super(IndexView, self).__init__(**kwargs)
+        self.data = {
+            "first_name": "Denis",
+            "last_name": "Svistunov",
+            "date_birth": "1991-04-19",
+            "bio": "Lorem ipsum dolor sit amet.",
+            "email": "d.svistunov1991@gmail.com",
+            "jabber": "d.svistunov@42cc.co",
+            "skype": "d.svistunov",
+            "other_contacts": "+380632028013"
+        }
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['data'] = self.data
+        return context
