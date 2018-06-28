fd3dd4b024ab82ed0e782931efa54f0ddfa7ad37
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-28 14:56:13 +0300

    ticket:8 EditView returns form with model inst, edit page takes form

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -47,6 +47,33 @@ class RequestsView(JSONResponseMixin, TemplateView):
         return response
 
 
-class EditView(FormView):
+class AjaxableResponseMixin(object):
+
+    def render_to_json_response(self, context, **response_kwargs):
+        data = json.dumps(context)
+        response_kwargs['content_type'] = 'application/json'
+        return HttpResponse(data, **response_kwargs)
+
+    def form_invalid(self, form):
+        response = super(AjaxableResponseMixin, self).form_invalid(form)
+        if self.request.is_ajax():
+            return self.render_to_json_response(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        response = super(AjaxableResponseMixin, self).form_valid(form)
+        if self.request.is_ajax():
+            data = {'msg': 'Changes have been saved'}
+            return self.render_to_json_response(data)
+        else:
+            return response
+
+
+class EditView(AjaxableResponseMixin, FormView):
     template_name = 'edit.html'
     form_class = ProfileForm
+
+    def get_form(self, form_class):
+        profile = Profile.objects.first()
+        return form_class(instance=profile, **self.get_form_kwargs())
