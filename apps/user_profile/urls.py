78aa7bcb48bd0ac90d8d1a098260ebb6638624ad
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-28 14:07:27 +0300

    ticket:8 added rouoting for EditView

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
-from .views import IndexView, RequestsView
+from .views import IndexView, RequestsView, EditView
 
 
 urlpatterns = patterns(
     '',
 
+    url(r'^edit/$', EditView.as_view(), name='edit'),
     url(r'^requests/$', RequestsView.as_view(), name='requests'),
     url(r'^$', IndexView.as_view(), name='index'),
 )
