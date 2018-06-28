471dcb202f159757303e50b0549e0704aea26a65
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-28 18:41:12 +0300

    ticket:8 maked routing for auth

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth import views as auth_views
 from .views import IndexView, RequestsView, EditView
 
 
 urlpatterns = patterns(
     '',
 
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^edit/$', EditView.as_view(), name='edit'),
     url(r'^requests/$', RequestsView.as_view(), name='requests'),
     url(r'^$', IndexView.as_view(), name='index'),
