81fa76f9c43f6a36a91eedf141fb374418626748
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-28 19:10:47 +0300

    ticket:8 fixed to flake8

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -6,7 +6,8 @@ from .views import IndexView, RequestsView, EditView
 urlpatterns = patterns(
     '',
 
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^login/$', auth_views.login,
+        {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^edit/$', EditView.as_view(), name='edit'),
     url(r'^requests/$', RequestsView.as_view(), name='requests'),
