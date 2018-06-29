cfebe82290c16643d7a355b1a5b03ceebfe73dac
dsvistunov <d.svistuno0v1991@gmail.com>
2018-06-29 15:51:42 +0300

    ticket:8 handeled priority

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
+    // Requests
+    var prior = 0;
+    $("#prior").click(function () {
+        if ($("#prior").val()=='Prior 1') {
+            prior = 0;
+            $("#prior").attr('value', 'Prior 0');
+        }
+        else {
+            prior = 1;
+            $("#prior").attr('value', 'Prior 1');
+        }
+    });
     setInterval(function () {
         $.ajax({
             url: '/requests/',
             method: 'get',
             success: function (data) {
                 var html = '';
-
+                if (prior) {
+                    data = Object.values(data).reverse();
+                }
                 $.each(data, function (key, value) {
                     key++;
                     html += "<a href='" + value.fields.http_inf + "'>" + key + ". " + value.fields.http_inf + "</a><br>"
