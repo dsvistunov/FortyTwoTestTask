a2dfb2d24c2ce7e38ab5741ad807863a33050ddb
dsvistunov <d.svistuno0v1991@gmail.com>
2018-07-13 21:34:34 +0300

    fixed

--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
+
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
